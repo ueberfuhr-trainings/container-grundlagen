0a2743c5dd9f5c6f94491ff74d201211384912ef
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2025-06-23 15:35:26 +0200

    Create landing page and a script to create and run an NGINX container with the landing page

--- loesungen/01-nginx/run-nginx.sh
+++ loesungen/01-nginx/run-nginx.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# on Windows Git Bash, use $(pwd -W)!
-docker run \
-  --rm \
-  -v "$(pwd)/index.html:/usr/share/nginx/html/index.html" \
-  -p8080:80 \
-  nginx:1.27.5
+# Name of the container
+CONTAINER_NAME=temp_nginx
+
+# Create a container in the background from the image
+docker container create --name $CONTAINER_NAME -p8080:80 nginx:1.27.5
+
+# Copy the file into the container
+docker container cp index.html $CONTAINER_NAME:/usr/share/nginx/html/index.html
+
+# Start the container, execute the script, and remove the container afterward
+docker container start -a $CONTAINER_NAME
+
+# Clean up: remove the container
+docker container remove $CONTAINER_NAME
