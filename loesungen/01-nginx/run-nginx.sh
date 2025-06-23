b1f56ae7ca954164c28096aff863c06de4cd272a
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2025-06-23 15:56:43 +0200

    Switch to volume mounting.

--- loesungen/01-nginx/run-nginx.sh
+++ loesungen/01-nginx/run-nginx.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# Name of the container
-CONTAINER_NAME=temp_nginx
-
-# Create a container in the background from the image
-docker container create --name $CONTAINER_NAME -p8080:80 nginx:1.27.5
-
-# Copy the file into the container
-docker container cp index.html $CONTAINER_NAME:/usr/share/nginx/html/index.html
-
-# Start the container, execute the script, and remove the container afterward
-docker container start -a $CONTAINER_NAME
-
-# Clean up: remove the container
-docker container remove $CONTAINER_NAME
+docker run \
+  --rm \
+  -v "$(pwd)/index.html:/usr/share/nginx/html/index.html" \
+  -p8080:80 \
+  nginx:1.27.5
