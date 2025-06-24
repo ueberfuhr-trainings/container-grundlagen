b9b391bed48793e30aa04ba0f111fd75a85c79e8
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2025-06-24 12:41:31 +0200

    Create PostgreSQL container with helloworld table.

---
+++ loesungen/03-postgres/run-database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run \
+  --rm \
+  -v "$(pwd)/schema.sql:/docker-entrypoint-initdb.d/1-schema.sql" \
+  -p5432:5432 \
+  -e POSTGRES_DB=helloworld \
+  -e POSTGRES_USER=user \
+  -e POSTGRES_PASSWORD=password \
+  postgres:17.5
