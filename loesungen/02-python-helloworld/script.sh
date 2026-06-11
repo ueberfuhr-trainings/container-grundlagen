4294b689d088e77ce127d579ed1eeb06a6026138
Ralf Ueberfuhr <ralf.ueberfuhr@atvantage.com>
2026-06-11 15:53:12 +0200

    refactor: mount working directory and set container path for python execution

--- loesungen/02-python-helloworld/script.sh
+++ loesungen/02-python-helloworld/script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# on Windows Git Bash, use $(pwd -W)!
+# on Windows Git Bash, use MSYS_NO_PATHCONV=1
 docker run \
   --rm \
-  -v "$(pwd)/hello.py:/hello.py" \
+  -v "$(pwd)/:/usr/src/myapp" \
+  -w "/usr/src/myapp" \
   python:3-slim \
-  python /hello.py
+  python hello.py
